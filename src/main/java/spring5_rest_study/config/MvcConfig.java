7a885b2c3b297c5c53446cab5cb7276be26eb212
mskim <net94@naver.com>
2021-05-28 12:48:33 +0900

    불필요제거

--- src/main/java/spring5_rest_study/config/MvcConfig.java
+++ src/main/java/spring5_rest_study/config/MvcConfig.java
@@ -4,16 +4,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -37,29 +33,6 @@ public class MvcConfig implements WebMvcConfigurer{
     }
     
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/main").setViewName("main");
-    }
-
-    @Bean
-    public MessageSource messageSource() { // 빈의 아이디를 반드시 "messageSource"로 지정
-        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
-        ms.setBasename("message.label");
-        ms.setDefaultEncoding("UTF-8");
-        return ms;
-    }
-    
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(authCheckInterceptor()).addPathPatterns("/edit/**");
-//    }
-//    
-//    @Bean
-//    public AuthCheckInterceptor authCheckInterceptor() {
-//        return new AuthCheckInterceptor();
-//    }
-
-    @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
@@ -70,4 +43,5 @@ public class MvcConfig implements WebMvcConfigurer{
             .build();
         converters.add(0, new MappingJackson2HttpMessageConverter(objectMapper));
     }
+    
 }
